1cf913280e019581038cb36d0f6d025e24d6bb79
Elmer Ibayan <eibayan@gmail.com>
2017-11-01 16:53:57 +1100

    modify about content

--- src/About.js
+++ src/About.js
@@ -5,11 +5,29 @@ export default class About extends Component {
   render() {
     return (
       <div className="md-grid">
-        <h2 className="md-cell md-cell--12 md-text-container">
-          About 
-        </h2>
-        <Slider id="page-3-slider" className="md-cell md-cell--12" />
-        <h3 className="md-cell md-cell--8 md-text-container">This is the about page...</h3>
+        <h2 className="md-cell md-cell--12 md-text-container">About: MyReeds</h2>
+        <p> 
+          <h4> An application that allows user to organize books based on reading activity. 
+          There are 3 status or shelves where each book can be 
+          placed namely: "Currently Reading", "Want To Read", "Read". 
+          There's also a search capability that dynamically filter books 
+          by matching query with status, title or author.</h4>
+
+          <h5> To Use </h5>
+          <ul>
+            <li> clone this repo</li>
+            <li> run "npm install" on terminal to install dependencies </li>
+            <li> run "npm start" </li>
+          </ul>
+          
+          <h5> Technology </h5>
+            <li> :Using create-react-app: ReactJS, React-md, React Router 4 </li>
+
+          <h5> License / Credits </h5>
+            <li> Udacity React Nano Degree </li>
+            <li> create-react-app </li>
+            <li> react-md-with-router4 example </li>
+        </p>
       </div>
     )
   }
